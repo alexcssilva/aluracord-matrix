78a9104a7d568f57b1db879d6629981f233486f9
Alex Silva <alexcssilva@gmail.com>
2022-02-02 09:14:15 -0300

    Alura - Aula 5 finalizada

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,41 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import React from 'react';
-import { useRouter } from "next/router";
-import appConfig from "../config.json";
+import { useRouter } from 'next/router';
+import appConfig from '../config.json';
 
-function Title(props) {
-  const Tag = props.tag || "h1";
+function Titulo(props) {
+  const Tag = props.tag || 'h1';
   return (
     <>
       <Tag>{props.children}</Tag>
-        <style jsx>{`
-        ${Tag} {
-          color: ${appConfig.theme.colors.neutrals['000']};
-          font-size: 24px;
-          font-weigt: 600;
-        }
-        `}</style>
+      <style jsx>{`
+            ${Tag} {
+                color: ${appConfig.theme.colors.neutrals['000']};
+                font-size: 24px;
+                font-weight: 600;
+            }
+            `}</style>
     </>
-  )
+  );
 }
 
 // Componente React
-
 // function HomePage() {
-//   //JSX
-//   return (
-//     <div style={{ backgroundColor: 'black'}}>
-//       <GlobalStyle />
-//       <Title tag="h1">Boas vindas de volta!</Title>
-//       <h2>Discord - Alura Matrix</h2>
-//     </div>
-//   ) 
+//     // JSX
+//     return (
+//         <div>
+//             <GlobalStyle />
+//             <Titulo tag="h2">Boas vindas de volta!</Titulo>
+//             <h2>Discord - Alura Matrix</h2>
+//         </div>
+//     )
 // }
+// export default HomePage
 
-// export default HomePage;
-
-function HomePage() {
- // const username = 'alexcssilva';
+export default function PaginaInicial() {
+  // const username = 'alexcssilva';
   const [username, setUsername] = React.useState('alexcssilva');
-  const route = useRouter();
+  const roteamento = useRouter();
 
   return (
     <>
@@ -67,43 +65,45 @@ function HomePage() {
           {/* Formulário */}
           <Box
             as="form"
-            onSubmit={function (infoEvent) {
-              infoEvent.preventDefault();
-              console.log('Alguem submeteu');
-              route.push('/chat');
-              // window.location.href = "/chat"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              console.log('Alguém submeteu o form');
+              // roteamento.push('/chat?username=' + username);
+              roteamento.push(`/chat?username=${username}`);
+              // window.location.href = '/chat';
             }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            <Title tag="h2">Boas vindas de volta!</Title>
+            <Titulo tag="h2">Boas vindas de volta!</Titulo>
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
 
             {/* <input
-              type="text"
-              value={username}
-              onChange={ function (event) {
-                // Onde está o valor?
-                const valor = event.target.value;
-                // Trocar o valor da variavel
-                // através do React e avise quem precise
-                setUsername(valor)
-              }}
-            /> */}
-
+                            type="text"
+                            value={username}
+                            onChange={function (event) {
+                                console.log('usuario digitou', event.target.value);
+                                // Onde ta o valor?
+                                const valor = event.target.value;
+                                // Trocar o valor da variavel
+                                // através do React e avise quem precisa
+                                setUsername(valor);
+                            }}
+                        /> */}
             <TextField
               value={username}
-              onChange={ function (event) {
-                  // Onde está o valor?
-                  const valor = event.target.value;
-                  // Trocar o valor da variavel
-                  // através do React e avise quem precise
-                  setUsername(valor)
-                }}
+              onChange={function (event) {
+                console.log('usuario digitou', event.target.value);
+                // Onde ta o valor?
+                const valor = event.target.value;
+                // Trocar o valor da variavel
+                // através do React e avise quem precisa
+                setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -170,5 +170,3 @@ function HomePage() {
     </>
   );
 }
-
-export default HomePage;
